e228bff294e88b6557763542f2d0dad72bf3a81b
Felipe César <58090526+felipecesargomes@users.noreply.github.com>
2021-03-19 18:33:40 -0300

    Update destravando-apt-get-install.sh

--- destravando-lock-frontend/destravando-apt-get-install.sh
+++ destravando-lock-frontend/destravando-apt-get-install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# OCORRE QUANDO O APT-GET INSTALL É ENCERRADO DE FORMA INESPERADA, é necessário matar o processo ou apagar os arquivos de trava.
+
 sudo rm /var/lib/apt/lists/lock
 sudo rm /var/lib/dpkg/lock
 sudo rm /var/lib/dpkg/lock-frontend
 sudo rm /var/cache/apt/archives/lock
+
+# RECONFIGURA o dpkg
 
 sudo dpkg --configure -a
